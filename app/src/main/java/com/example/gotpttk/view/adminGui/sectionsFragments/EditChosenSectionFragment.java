14f0a6fe05584e8a6e8618e2ab4be4787561c2a9
PepeKwapien <p.kwapien@op.pl>
2021-01-22 10:58:51 +0100

    przygotowanie kodu edit/remove chosen section

--- app/src/main/java/com/example/gotpttk/view/adminGui/sectionsFragments/EditChosenSectionFragment.java
+++ app/src/main/java/com/example/gotpttk/view/adminGui/sectionsFragments/EditChosenSectionFragment.java
@@ -7,18 +7,58 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.gotpttk.R;
+import com.example.gotpttk.model.dbHelper.DatabaseHelper;
+import com.example.gotpttk.model.dbModels.Section;
 
 
 public class EditChosenSectionFragment extends Fragment
 {
+    Button btnSaveSectionChanges;
+    EditText etEditSectionStart;
+    EditText etEditSectionEnd;
+    EditText etEditSectionLength;
+    EditText etEditSectionMountain;
+    EditText etEditSectionPointsTo;
+    EditText etEditSectionPointsFrom;
+    EditText etEditSectionDesc;
+    long SectionId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_chosen_section, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_chosen_section, container, false);
+        SectionId = getArguments().getLong("id");
+        DatabaseHelper databaseHelper = new DatabaseHelper(view.getContext());
+        //Uzupelnic danymi - zamiast new section dac getSection(id)
+        Section current = new Section();
+        btnSaveSectionChanges = (Button)view.findViewById(R.id.buttonEditSection);
+        etEditSectionStart = view.findViewById(R.id.editTextStartingSpot);
+        //set
+        etEditSectionEnd = view.findViewById(R.id.editTextEndingSpot);
+
+        etEditSectionLength = view.findViewById(R.id.editTextLength);
+
+        etEditSectionMountain = view.findViewById(R.id.editTextMountainRange);
+
+        etEditSectionPointsTo = view.findViewById(R.id.editTextPoints);
+
+        etEditSectionPointsFrom = view.findViewById(R.id.editTextReturnPoints);
+
+        etEditSectionDesc = view.findViewById(R.id.editTextSectionDesc);
+
+        btnSaveSectionChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Update odcinka!
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/gotpttk/view/adminGui/sectionsFragments/RemoveChosenSectionFragment.java
+++ app/src/main/java/com/example/gotpttk/view/adminGui/sectionsFragments/RemoveChosenSectionFragment.java
@@ -7,17 +7,58 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.gotpttk.R;
+import com.example.gotpttk.model.dbHelper.DatabaseHelper;
+import com.example.gotpttk.model.dbModels.Section;
 
 
 public class RemoveChosenSectionFragment extends Fragment
 {
+    Button btnRemoveSection;
+    EditText tvRemoveSectionStart;
+    EditText tvRemoveSectionEnd;
+    EditText tvRemoveSectionLength;
+    EditText tvRemoveSectionMountain;
+    EditText tvRemoveSectionPointsTo;
+    EditText tvRemoveSectionPointsFrom;
+    EditText tvRemoveSectionDesc;
+    long SectionId;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_remove_chosen_section, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_chosen_section, container, false);
+        SectionId = getArguments().getLong("id");
+        DatabaseHelper databaseHelper = new DatabaseHelper(view.getContext());
+        //Uzupelnic danymi - zamiast new section dac getSection(id)
+        Section current = new Section();
+        btnRemoveSection = (Button)view.findViewById(R.id.buttonRemoveSection);
+        tvRemoveSectionStart = view.findViewById(R.id.textViewStartingSpotValue);
+        //set
+        tvRemoveSectionEnd = view.findViewById(R.id.textViewEndingSpotValue);
+
+        tvRemoveSectionLength = view.findViewById(R.id.textViewLengthValue);
+
+        tvRemoveSectionMountain = view.findViewById(R.id.textViewMountainRangeValue);
+
+        tvRemoveSectionPointsTo = view.findViewById(R.id.textViewPointsValue);
+
+        tvRemoveSectionPointsFrom = view.findViewById(R.id.textViewReturnPointsValue);
+
+        tvRemoveSectionDesc = view.findViewById(R.id.textViewSectionDesc);
+
+        btnRemoveSection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Delete odcinka!
+            }
+        });
+
+        return view;
     }
 }
